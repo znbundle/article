ddd048937a824ad6a806ee58352b14337cd0b1ef
Vitaliy <theyamshikov@yandex.ru>
2022-06-20 12:12:56 +0600

    upd

--- src/Domain/Repositories/Relations/PostRelation.php
+++ src/Domain/Repositories/Relations/PostRelation.php
@@ -29,45 +29,45 @@ class PostRelation
         return ['id'];
     }
 
-    public function relations()
-    {
-        return [
-            'category' => [
-                /*'type' => RelationEnum::ONE,
-                'field' => 'categoryId',
-                'foreign' => [
-                    'model' => $this->categoryRepository,
-                    'field' => 'id',
-                ],*/
-                'type' => RelationEnum::CALLBACK,
-                'callback' => function (Collection $collection) {
-                    $m2m = new OneToOne;
-                    //$m2m->selfModel = $this;
-
-                    $m2m->foreignModel = $this->categoryRepository;
-                    $m2m->foreignField = 'categoryId';
-                    $m2m->foreignContainerField = 'category';
-
-                    $m2m->run($collection);
-                },
-            ],
-            'tags' => [
-                'type' => RelationEnum::CALLBACK,
-                'callback' => function (Collection $collection) {
-                    $m2m = new ManyToMany;
-                    $m2m->selfModel = $this;
-                    $m2m->selfField = 'postId';
-
-                    $m2m->viaModel = $this->tagPostRepository;
-
-                    $m2m->foreignModel = $this->tagRepository;
-                    $m2m->foreignField = 'tagId';
-                    $m2m->foreignContainerField = 'tags';
-
-                    $m2m->run($collection);
-                },
-            ],
-        ];
-    }
+//    public function relations()
+//    {
+//        return [
+//            'category' => [
+//                /*'type' => RelationEnum::ONE,
+//                'field' => 'categoryId',
+//                'foreign' => [
+//                    'model' => $this->categoryRepository,
+//                    'field' => 'id',
+//                ],*/
+//                'type' => RelationEnum::CALLBACK,
+//                'callback' => function (Collection $collection) {
+//                    $m2m = new OneToOne;
+//                    //$m2m->selfModel = $this;
+//
+//                    $m2m->foreignModel = $this->categoryRepository;
+//                    $m2m->foreignField = 'categoryId';
+//                    $m2m->foreignContainerField = 'category';
+//
+//                    $m2m->run($collection);
+//                },
+//            ],
+//            'tags' => [
+//                'type' => RelationEnum::CALLBACK,
+//                'callback' => function (Collection $collection) {
+//                    $m2m = new ManyToMany;
+//                    $m2m->selfModel = $this;
+//                    $m2m->selfField = 'postId';
+//
+//                    $m2m->viaModel = $this->tagPostRepository;
+//
+//                    $m2m->foreignModel = $this->tagRepository;
+//                    $m2m->foreignField = 'tagId';
+//                    $m2m->foreignContainerField = 'tags';
+//
+//                    $m2m->run($collection);
+//                },
+//            ],
+//        ];
+//    }
 
 }
