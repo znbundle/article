7cb2a4431b86ab4ef6d52cf5aede3b291703f8e6
Vitaliy <theyamshikov@yandex.ru>
2022-06-26 20:32:24 +0600

    upd

--- src/Symfony4/Web/Controllers/ArticleController.php
+++ src/Symfony4/Web/Controllers/ArticleController.php
@@ -14,7 +14,7 @@ use ZnBundle\Notify\Domain\Interfaces\Services\FlashServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use ZnLib\Web\Components\Controller\Helpers\WebQueryHelper;
+use ZnLib\Web\Controller\Helpers\WebQueryHelper;
 
 class ArticleController extends AbstractController
 {
